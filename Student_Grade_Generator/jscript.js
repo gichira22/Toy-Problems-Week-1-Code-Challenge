d4ceb16b965cc2ae9855993b9cd29444f9aadc90
Virginia Gichira <gichiravirginia@gmail.com>
2024-03-25 13:47:17 +0300

    add

--- Student_Grade_Generator/jscript.js
+++ Student_Grade_Generator/jscript.js
@@ -1,3 +1,29 @@
+// Creates a form validation function //
+function validateFormGrader() {
+  let ht = parseInt(document.getElementById("ht").value);
+  let css = parseInt(document.getElementById("css").value);
+  let js = parseInt(document.getElementById("js").value);
+  let py = parseInt(document.getElementById("py").value);
+  let sq = parseInt(document.getElementById("sq").value);
+
+  // Checks if the input fields are empty //
+
+
+  if (ht === "" ||  css=== "" || js === "" || py === "" || sq === "") {
+    document.getElementById("error").innerText = "Input values required.";
+    return false;
+  }
+  // Checks if the values are within range //
+  if (
+  ht < 0 || ht > 100 || css < 0 || css > 100 || js < 0 || js > 100 || py < 0 || py > 100 || sq < 0 || sq > 100) {
+  document.getElementById("error").innerText = "Values must be within 0 and 100.";
+  return false;
+  }
+  // Clears any errors when all checks pass //
+  document.getElementById("error").innerText = "";
+  Generate_Results();
+  return false;
+}
 function Generate_Results(){
 let ht = document.querySelector("#ht").value;
 let css = document.querySelector("#css").value;
